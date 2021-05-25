2a7c246ea9187f4d3da70f5dc7a74fd7cb6f8564
claudioshiver <Sxiver2912>
2021-05-25 12:16:51 +0200

    fix: tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,34 @@
 const palette = {
-  'primary': '#6579d6'
+    'primary': '#6579d6'
 };
 
 module.exports = {
-  purge: process.env.NODE_ENV === 'production' ? [
-      './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'
-  ] : [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      height: {
-        screen2x: '200vh'
-      },
-      scale: {
-        '80': '.8',
-        '120': '1.2'
-      },
-      color: palette,
-      backgroundColor: palette,
-      borderColor: palette,
-      textColor: palette
-    }
-  },
-  variants: {
-    extend: {
-      transform: ['hover', 'active', 'focus'],
-      ringWidth: ['hover'],
-      ringColor: ['hover']
-    }
-  },
-  plugins: [],
+    purge: process.env.NODE_ENV === 'production' ? [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ] : [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            height: {
+                screen2x: '200vh'
+            },
+            scale: {
+                '80': '.8',
+                '120': '1.2'
+            },
+            color: palette,
+            backgroundColor: palette,
+            borderColor: palette,
+            textColor: palette
+        }
+    },
+    variants: {
+        extend: {
+            transform: ['hover', 'active', 'focus'],
+            ringWidth: ['hover'],
+            ringColor: ['hover']
+        }
+    },
+    plugins: [],
 }
